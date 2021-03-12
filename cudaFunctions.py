e7c2b1346392cde55db76e6c4d07a57f4a3c6c36
Jake <jake.pollard95@gmail.com>
2021-03-11 18:14:00 -0800

    working on accumulating

--- cudaFunctions.py
+++ cudaFunctions.py
@@ -3,7 +3,7 @@ import pycuda.driver as drv
 import numpy as np
 
 from pycuda.compiler import SourceModule
-mod = SourceModule("""
+mmod = SourceModule("""
 __global__ void multiply_them(float *dest, float *a, float *b)
 {
   const int i = threadIdx.x;
@@ -11,13 +11,16 @@ __global__ void multiply_them(float *dest, float *a, float *b)
 }
 """)
 
-cuMean = SourceModule("""
-__global__ void cudaMean(float *dest, float labels, float *b)
+mod = SourceModule("""
+__global__ void accumulate(int *out, int *in)
 {
-  const int i = threadIdx.x;
-  dest[i] = a[i] * b[i];
-}
+    const int i = threadIdx.x + blockIdx.x*blockDim.x;
+    out[i] = out[i] + in[i];
+} 
 """)
 
-multiply_them = mod.get_function("multiply_them")
-cudaMean = cuMean.get_function("cudaMean")
+
+
+
+multiply_them = mmod.get_function("multiply_them")
+accumulate = mod.get_function("accumulate")
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import pycuda.autoinit
-import pycuda.driver as drv
+import pycuda.driver as cuda
 import numpy as np
 import csv
 from pycuda.compiler import SourceModule
@@ -15,7 +15,7 @@ b = np.random.randn(400).astype(np.float32)
 
 dest = np.zeros_like(a)
 multiply_them(
-        drv.Out(dest), drv.In(a), drv.In(b),
+        cuda.Out(dest), cuda.In(a), cuda.In(b),
         block=(400,1,1), grid=(1,1))
 
 
@@ -28,17 +28,17 @@ def readData():
         next(it)
 
         train = [row for row in it]
-        trainLabels = np.array([row[0] for row in train],dtype=np.float32)
-        trainSamples = np.array([row[1:] for row in train],dtype=np.float32)
-        train  = trainLabels,trainSamples)
+        trainLabels = np.array([row[0] for row in train],dtype=np.int32)
+        trainSamples = np.array([row[1:] for row in train],dtype=np.int32)
+        train  = (trainLabels,trainSamples)
 
     with open(testData,'r') as fin:
-        it = csv.read(fin,delimeter)
+        it = csv.reader(fin,delimiter=',')
         next(it)
 
         test = [row for row in it]
-        testLabels = np.array([row[0] for row in test],dtype=np.float32)
-        testSamples = np.array([row[1:] for row in test],dtype=np.float32)
+        testLabels = np.array([row[0] for row in test],dtype=np.int32)
+        testSamples = np.array([row[1:] for row in test],dtype=np.int32)
         test  = (testLabels,testSamples)
 
     return (train,test)
@@ -49,14 +49,55 @@ def readData():
 train,test = readData()
 trainLabels, trainSamples = train
 testLabels, testSamples = test
+#start with JUST integers
 
 #init to zero, means
-means = np.zeros((NUM_CLASSES,len(labels)),dtype=np.float32)
+means = np.zeros((NUM_CLASSES,len(trainSamples[0])),dtype=np.int32)
+#cudaRet = cuda.mem_alloc(means.nbytes)
+#cudaSamples = cuda.mem_alloc(trainSamples.nbytes)
+
+#create streams
+streams = []
+inputs = []
+rets = []
+counts = [0 for _ in range(NUM_CLASSES)]
+for i in range(NUM_CLASSES):
+    streams.append(cuda.Stream())
+    inputs.append(cuda.mem_alloc(trainSamples[0].nbytes))
+    rets.append(cuda.mem_alloc(trainSamples[0].nbytes))
+
+
+#send all means over
+for i in range(NUM_CLASSES):
+    cuda.memcpy_htod_async(rets[i],means[i],stream=streams[i])
+
+## accumulate everything
+for vector,label in zip(trainSamples,trainLabels):
+    counts[label-1]+=1
+    cuda.memcpy_htod_async(inputs[label-1],vector,stream=streams[label-1])
+    #temporary, set back to 32 later
+    accumulate(rets[label-1],inputs[label-1],block=(28,1,1),grid=(28,1),stream=streams[label-1])
+    
+
+for i in range(NUM_CLASSSES):
+    cuda.memcpy_dtoh_async(means[i],ret[i],stream=streams[i])
+
+for stream in streams:
+    stream.synchronize()
+for i in range(NUM_CLASSES):
+    print(means[i])
+
+
+
+
+    
+
+
+
 
 #malloc
-cudaRet = cuda.mem_alloc(ret.nbytes)
-cudaLabels = cuda.mem_alloc(trainLabels.nbytes)
-cudaSamples = cuda.mem_alloc(trainSamples.nbytes)
+print(trainSamples.shape)
+print(trainLabels.shape)
 
 #mem copy to GPU
 cuda.memcpy_htod(cudaRet,means)
@@ -64,6 +105,7 @@ cuda.memcpy_htod(cudaLabels,trainLabels)
 cuda.memcpy_htod(cudaSamples,trainSamples)
 
 #call function here..
+#cudaMean(ret,drv.In(samples),drv.In(labels),block=(,,),grid(,))
 
 #bring mean back to RAM
 
@@ -73,4 +115,3 @@ cuda.memcpy_htod(cudaSamples,trainSamples)
 
 
 
-cudaMean(ret,drv.In(samples),drv.In(labels),block=(,,),grid(,))
